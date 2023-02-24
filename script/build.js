78681872a258b6f09740653cf002a749f3fdfe0f
NOBUYUK1 <122588734+NOBUYUK1@users.noreply.github.com>
2023-02-24 18:23:53 +0000

    README atualizado

---
+++ script/build.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const orderedEmojiData = fs.readFileSync('./emoji-order.txt', 'utf-8')
+const groupedEmojiData = fs.readFileSync('./emoji-group.txt', 'utf-8')
+const VARIATION_16 = String.fromCodePoint(0xfe0f)
+const SKIN_TONE_VARIATION_DESC = /\sskin\stone(?:,|$)/
+
+// Final data holder
+const orderedEmoji = []
+const dataByEmoji = {}
+const dataByGroup = []
+const emojiComponents 
