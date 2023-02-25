beaa1bb65496750003e730c4cd3cd04070785b1c
NOBUYUK1 <122588734+NOBUYUK1@users.noreply.github.com>
2023-02-25 10:49:02 +0000

    SEO

--- script/build.js
+++ script/build.js
@@ -8,4 +8,21 @@ const SKIN_TONE_VARIATION_DESC = /\sskin\stone(?:,|$)/
 const orderedEmoji = []
 const dataByEmoji = {}
 const dataByGroup = []
-const emojiComponents 
+const emojiComponents = {}
+
+// The group data tells if the emoji is one of the following:
+//   component
+//   fully-qualified
+//   minimally-qualified
+//   unqualified
+//
+// We only want fully-qualified emoji in the output data
+
+// # group: Smileys & Emotion
+//          |1--------------|
+//
+const GROUP_REGEX = /^#\sgroup:\s(?<name>.+)/
+
+// 1F646 200D 2640 FE0F                       ; fully-qualified     # 🙆‍♀️ E4.0 woman gesturing OK
+//                                              |1------------|      |2--||3-| |4---------------|
+// 1F469 200D 1F469 200D 1F467
